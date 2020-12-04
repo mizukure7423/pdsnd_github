65b49282dd5e40bb93857ebf83cb0a801133ec75
Mizukure Shizuka <mizukure.s@mazda.co.jp>
2020-12-04 23:37:19 +0900

    {} .format(common_hour)

--- bikeshare.py
+++ bikeshare.py
@@ -109,7 +109,7 @@ def time_stats(df):
 
     # display the most common start hour
     common_hour = df['hour'].mode()[0]
-    print("The most common hour is: " + str(common_hour))
+    print("The most common hour is: {}"  .format(common_hour))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
